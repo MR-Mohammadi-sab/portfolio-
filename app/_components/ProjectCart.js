7d1af67c04ad977790bad06da7be1490e020cf52
mr-mohammadi-sab <asf423796@gmail.com>
2026-06-22 11:20:50 -0700

    add many of project

--- app/_components/ProjectCart.js
+++ app/_components/ProjectCart.js
@@ -8,7 +8,7 @@ export default function ProjectCart() {
       {/* Title */}
       <div className="text-center mb-10">
         <h2 className="text-3xl font-bold text-text-light dark:text-text-dark">
-          My Projects
+          My Projects {datas?.length}
         </h2>
         <p className="text-sm mt-2 text-text-secondary-light dark:text-text-secondary-dark">
           A collection of things I’ve built while learning and practicing frontend development

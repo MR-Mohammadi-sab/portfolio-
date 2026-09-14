15d985106ff4cceb9934c04b68c995004c151adf
mr-mohammadi-sab <asf423796@gmail.com>
2026-09-14 01:47:19 -0700

    add blur

--- app/_components/Cv.js
+++ app/_components/Cv.js
@@ -3,7 +3,7 @@ import cv from "@/public/cv.png";
 export default function CVViewer() {
   return (
     <div className="flex flex-col items-center ">
-      <Image src={cv} quality={80} alt="my pdf" />
+      <Image src={cv} quality={80} alt="my pdf" placeholder="blur" />
     </div>
   );
 }
